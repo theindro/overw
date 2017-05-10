4d37ebf5db48db09b354012e000dbc5c03680f66
indro <indro.malleus@khk.ee>
2017-05-10 17:24:11 +0300

    Parandatud profiili uuendamise funktsiooni.

--- functions.php
+++ functions.php
@@ -347,28 +347,26 @@ function updateProfile()
         exit('Battletag not found on API');
     }
 
-    $delete_user = $wpdb->query("DELETE FROM wp_ranking where battle_tag = '$battle_tag'");
-    $delete_hero = $wpdb->query("DELETE FROM wp_heroes where battle_tag_id = $battle_tag_id");
-    $delete_medals = $wpdb->query("DELETE FROM wp_medals where battle_tag_id = $battle_tag_id");
-    $delete_hero_data = $wpdb->query("DELETE FROM wp_hero_avg_stats where battle_tag_id = $battle_tag_id");
+    //$delete_user = $wpdb->query("DELETE FROM wp_ranking where battle_tag = '$battle_tag'");
+    //$delete_hero = $wpdb->query("DELETE FROM wp_heroes where battle_tag_id = $battle_tag_id");
+    //$delete_medals = $wpdb->query("DELETE FROM wp_medals where battle_tag_id = $battle_tag_id");
+    //$delete_hero_data = $wpdb->query("DELETE FROM wp_hero_avg_stats where battle_tag_id = $battle_tag_id");
 
     preg_match("/(.*?)(?=[-])/", $battle_tag, $name);
 
     if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-//check ip from share internet
+    //check ip from share internet
         $ip = $_SERVER['HTTP_CLIENT_IP'];
     } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-//to check ip is pass from proxy
+    //to check ip is pass from proxy
         $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
     } else {
         $ip = $_SERVER['REMOTE_ADDR'];
     }
 
 
-// Insert API data to db.
+    // Update wp_ranking table
     $overall = array(
-        'battle_tag' => $battle_tag,
-        'name' => $name['0'],
         'lvl' => ($overall_stats->prestige * 100 + $overall_stats->level),
         'avatar' => $overall_stats->avatar,
         'rank' => $overall_stats->comprank,
@@ -378,19 +376,15 @@ function updateProfile()
         'ties' => $overall_stats->ties,
         'played' => $overall_stats->games,
         'last_updated' => date('Y-m-d H:i:s', current_time('timestamp', 0)),
-        'ip_address' => $ip,
-        'team_id' => $team_id
     );
 
-    $insert_overall = $wpdb->insert('wp_ranking', $overall);
-    $battle_tag_id = $wpdb->insert_id;
+    $update_wp_ranking = $wpdb->update('wp_ranking', $overall, array('battle_tag_id'=>$battle_tag_id));
 
     $obj_time = gmdate('H:i:s', floor($average_stats->objective_time_avg * 3600));
     $timeonfire = gmdate('H:i:s', floor($average_stats->time_spent_on_fire_avg * 3600));
 
-// Insert API user stats to db.
+    // Update wp_average_stats table
     $player_stats = array(
-        'battle_tag_id' => $battle_tag_id,
         'melee_final_blows' => $average_stats->melee_final_blows_avg,
         'time_spent_on_fire' => $timeonfire,
         'solo_kills' => $average_stats->solo_kills_avg,
@@ -403,31 +397,28 @@ function updateProfile()
         'eliminations' => $average_stats->eliminations_avg
     );
 
-    $insert_average_stats = $wpdb->insert('wp_average_stats', $player_stats);
+    $update_average_stats = $wpdb->update('wp_average_stats', $player_stats, array('battle_tag_id'=>$battle_tag_id));
 
     $player_medals = array(
-        'battle_tag_id' => $battle_tag_id,
         'gold' => $game_stats->medals_gold,
         'silver' => $game_stats->medals_silver,
         'bronze' => $game_stats->medals_bronze,
     );
 
-    $insert_medals = $wpdb->insert('wp_medals', $player_medals);
+    $update_medals = $wpdb->update('wp_medals', $player_medals, array('battle_tag_id'=>$battle_tag_id));
 
-// Add heroes playtime to database
+    // Add heroes playtime to database
     $all_heroes_playtime = $parsed_to_array['eu']['heroes']['playtime']['competitive'];
 
     foreach ($all_heroes_playtime as $hero_name => $playtime) {
-        $insert_heroes = $wpdb->insert('wp_heroes', ['hero_name' => $hero_name, 'playtime' => $playtime, 'battle_tag_id' => $battle_tag_id]);
+        $update_wp_heroes = $wpdb->update('wp_heroes', ['playtime' => $playtime], array('battle_tag_id'=>$battle_tag_id, 'hero_name' => $hero_name));
     }
 
-// Add heroes stats to database
+    // Add heroes stats to database
     $all_heroes_stats = $parsed_to_array['eu']['heroes']['stats']['competitive'];
 
     foreach ($all_heroes_stats as $hero_name => $hero_stats) {
         $data = array(
-            'battle_tag_id' => $battle_tag_id,
-            'hero_name' => $hero_name,
             'objective_time_average' => $hero_stats['average_stats']['objective_time_average'],
             'objective_kills_average' => $hero_stats['average_stats']['objective_kills_average'],
             'deaths_average' => $hero_stats['average_stats']['deaths_average'],
@@ -442,7 +433,7 @@ function updateProfile()
             'games_won' => $hero_stats['general_stats']['games_won'],
             'games_lost' => $hero_stats['general_stats']['games_lost'],
         );
-        $insert_heroes = $wpdb->insert('wp_hero_avg_stats', $data);
+        $update_wp_heroes_avg = $wpdb->update('wp_hero_avg_stats', $data, array('battle_tag_id'=>$battle_tag_id, 'hero_name' => $hero_name));
     }
 
     exit('Ok');
